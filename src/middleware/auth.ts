1322869ba28fd62f14a15adcf93f264fb3a5af53
Christopher Lang <65340138+chrisalxlng@users.noreply.github.com>
2025-12-16 18:22:14 +0100

    Separate single keycloak url env var into two

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,9 +2,17 @@ import type { GetVerificationKey } from "express-jwt";
 import { expressjwt as jwt } from "express-jwt";
 import jwksRsa from "jwks-rsa";
 
-const keycloakRealmUrl = `${process.env.KEYCLOAK_DOMAIN}/realms/${process.env.KEYCLOAK_APPS_REALM}`;
-if (!keycloakRealmUrl)
-	throw new Error("KEYCLOAK_DOMAIN and/or KEYCLOAK_APPS_REALM is not set");
+const keycloakIssuerRealmUrl = `${process.env.CLHUB_STORAGE_KEYCLOAK_ISSUER_URL}/realms/${process.env.KEYCLOAK_APPS_REALM}`;
+if (!keycloakIssuerRealmUrl)
+	throw new Error(
+		"CLHUB_STORAGE_KEYCLOAK_ISSUER_URL and/or KEYCLOAK_APPS_REALM is not set",
+	);
+
+const keycloakJwksRealmUrl = `${process.env.CLHUB_STORAGE_KEYCLOAK_JWKS_URL}/realms/${process.env.KEYCLOAK_APPS_REALM}`;
+if (!keycloakJwksRealmUrl)
+	throw new Error(
+		"CLHUB_STORAGE_KEYCLOAK_JWKS_URL and/or KEYCLOAK_APPS_REALM is not set",
+	);
 
 const keycloakAudience = process.env.CLHUB_STORAGE_KEYCLOAK_AUDIENCE;
 if (!keycloakAudience)
@@ -13,12 +21,12 @@ if (!keycloakAudience)
 const secret: GetVerificationKey = jwksRsa.expressJwtSecret({
 	cache: true,
 	rateLimit: true,
-	jwksUri: `${keycloakRealmUrl}/protocol/openid-connect/certs`,
+	jwksUri: `${keycloakJwksRealmUrl}/protocol/openid-connect/certs`,
 });
 
 export const authenticate = jwt({
 	secret,
 	audience: keycloakAudience,
-	issuer: `${keycloakRealmUrl}`,
+	issuer: `${keycloakIssuerRealmUrl}`,
 	algorithms: ["RS256"],
 });
